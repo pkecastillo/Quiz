9aa76e1da3093891f9bbad44e79b9a4c856b7bda
pkecastillo <pkecastillo@gmail.com>
2021-05-02 20:20:31 +0200

    register users and validations

--- src/app/components/usuario/register/register.component.ts
+++ src/app/components/usuario/register/register.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registrarForm: FormGroup
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.registrarForm = this.fb.group({
+      usuario: ['',[Validators.required, Validators.email]],
+      password: ['',[Validators.required, Validators.minLength(6)]],
+      repetirPassword: ['']
+    }, {validator: this.checkPassword})
+  }
 
   ngOnInit(): void {
+  }
+
+  registrar(){
+    console.log(this.registrarForm);
+  }
+
+  checkPassword(group: FormGroup): any{
+    const pass = group.controls.password?.value
+    const confirmPassword = group.controls.repetirPassword?.value
+    // console.log(pass);
+    // console.log(pass);
+    return pass === confirmPassword ? null : {notSame: true}
+
   }
 
 }
--- src/app/components/usuario/usuario-routing.module.ts
+++ src/app/components/usuario/usuario-routing.module.ts
@@ -1,3 +1,4 @@
+import { RegisterComponent } from './register/register.component';
 import { RecuperarPasswordComponent } from './recuperar-password/recuperar-password.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
@@ -5,7 +6,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'recuperarPassword', component: RecuperarPasswordComponent}
+  {path: 'recuperarPassword', component: RecuperarPasswordComponent},
+  {path: 'registrar', component: RegisterComponent}
 ];
 
 @NgModule({
